8466d705c4af695ed5c6821fd9f7d67f798a1a09
Felipe Azevedo Bosi <contato@felipebosi.com>
2025-08-14 22:46:42 -0300

    Add first component

---
+++ lib/Typography/Typography.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Typography } from './Typography';
+
+const meta = {
+  title: 'Components/Typography',
+  component: Typography,
+} satisfies Meta<typeof Typography>;
+
+export default meta;
+
+type Story = StoryObj<typeof Typography>;
+
+export const DefaultTypography: Story = {
+  args: {
+    color: 'primary',
+    size: 'body',
+    children: 'Text',
+  },
+};
---
+++ lib/Typography/Typography.tsx
@@ -0,0 +1,36 @@
+import type { ReactNode } from "react";
+import { colorClass, getComponent, sizeClass } from "./Typography.util";
+
+export type TColor =
+  'default' |
+  'primary' |
+  'secondary';
+
+export type TSize = 'body' |
+  'h1' |
+  'h2' |
+  'h3' |
+  'h4' |
+  'details';
+export interface TypographyProps {
+  color?: TColor;
+  size?: TSize;
+  className?: string;
+  as?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span' | 'details';
+  children: ReactNode;
+}
+
+export function Typography({
+  color = 'default',
+  size = 'body',
+  className,
+  children,
+  as,
+}: TypographyProps) {
+  const colorClassName = colorClass(color);
+  const sizeClassName = sizeClass(size);
+  const finalClassName = `${colorClassName} ${sizeClassName} ${className ? className : ''}`;
+  const Component = getComponent(size, as);
+
+  return <Component className={finalClassName}>{children}</Component>
+}
